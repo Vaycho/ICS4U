029ebacae9b9b55033ec25be8d41c95781675d01
vacym <vacym@DESKTOP-U8S7T8C>
2024-09-13 10:44:01 -0400

    Add project files.

---
+++ 100/100.cpp
@@ -0,0 +1,10 @@
+// 100.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main_100()
+{
+    std::cout << "Hello, ICS4U\n";
+    //Vacy Johnston-Henriquez
+}
